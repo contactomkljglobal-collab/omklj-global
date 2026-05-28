d62380e79b5c42b0aa5c378c38078abb94c2dd90
contactomkljglobal-collab <contact.omkljglobal@gmail.com>
2026-05-28 23:29:18 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,77 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+/* =========================
+   REGISTER USER
+========================= */
+
+const registerUser = async (req, res) => {
+
+  try {
+
+    const { name, email, password } = req.body;
+
+    /* CHECK USER */
+
+    const userExists = await User.findOne({ email });
+
+    if(userExists){
+
+      return res.status(400).json({
+        message: "User already exists"
+      });
+
+    }
+
+    /* HASH PASSWORD */
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword =
+      await bcrypt.hash(password, salt);
+
+    /* CREATE USER */
+
+    const user = await User.create({
+
+      name,
+      email,
+      password: hashedPassword
+
+    });
+
+    /* TOKEN */
+
+    const token = jwt.sign(
+
+      { id: user._id },
+
+      process.env.JWT_SECRET,
+
+      { expiresIn: "30d" }
+
+    );
+
+    res.status(201).json({
+
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token
+
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    });
+
+  }
+
+};
+
+module.exports = {
+  registerUser
+};

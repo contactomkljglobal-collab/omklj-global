51024dc14963da62ebe568956d577119a23d201e
contactomkljglobal-collab <contact.omkljglobal@gmail.com>
2026-05-28 23:02:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-/* MIDDLEWARE */
+/* =========================
+   MIDDLEWARE
+========================= */
 
 app.use(cors());
 app.use(express.json());
 
-/* TEST ROUTE */
+/* =========================
+   TEST ROUTE
+========================= */
 
 app.get("/", (req, res) => {
 
-res.send("OMKLJ Global Backend Running 🚀");
+  res.send("OMKLJ Backend Running 🚀");
 
 });
 
-/* PORT */
+/* =========================
+   PORT
+========================= */
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 
 });

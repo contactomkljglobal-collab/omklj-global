9c8f5257b2681e7f43303f089f019c0c7f6ff136
contactomkljglobal-collab <contact.omkljglobal@gmail.com>
2026-05-29 10:32:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  registerUser
-} = require("../controllers/authController");
+const { registerUser } = require("../controllers/authController");
 
-// Test route
-router.get("/", (req, res) => {
-  res.send("Auth Route Working");
-});
-
-// Test register route
-router.get("/register", (req, res) => {
-  res.send("Register Route Working");
-});
-
-// Actual register route
+// Register User
 router.post("/register", registerUser);
+
+// Test Route
+router.get("/", (req, res) => {
+    res.send("Auth API Running");
+});
 
 module.exports = router;

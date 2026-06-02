1e3b182415682d5236c47054915411ed5894defb
contactomkljglobal-collab <contact.omkljglobal@gmail.com>
2026-06-02 18:39:57 +0530

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,52 +1,20 @@
 const express = require("express");
 
-const {
-  getAllWorkers,
-  getPendingWorkers,
-  approveWorker,
-  rejectWorker
-} = require("../controllers/adminController");
-
 const router = express.Router();
 
-/*
-GET ALL WORKERS
-GET /api/admin/workers
-*/
+const {
+  getDashboardStats,
+  getAllWorkers
+} = require("../controllers/adminController");
+
+router.get(
+  "/dashboard",
+  getDashboardStats
+);
 
 router.get(
   "/workers",
   getAllWorkers
-);
-
-/*
-GET PENDING WORKERS
-GET /api/admin/pending-workers
-*/
-
-router.get(
-  "/pending-workers",
-  getPendingWorkers
-);
-
-/*
-APPROVE WORKER
-PUT /api/admin/approve/:id
-*/
-
-router.put(
-  "/approve/:id",
-  approveWorker
-);
-
-/*
-REJECT WORKER
-PUT /api/admin/reject/:id
-*/
-
-router.put(
-  "/reject/:id",
-  rejectWorker
 );
 
 module.exports = router;

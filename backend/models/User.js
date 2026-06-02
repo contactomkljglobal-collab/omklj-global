aead59cdfe160d4c14d2b7cadfd86c91a6e26729
contactomkljglobal-collab <contact.omkljglobal@gmail.com>
2026-06-02 15:55:07 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,26 +1,46 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
-  {
+{
     name: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
 
     email: {
-      type: String,
-      required: true,
-      unique: true
+        type: String,
+        required: true,
+        unique: true
     },
 
     password: {
-      type: String,
-      required: true
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: [
+            "worker",
+            "business",
+            "personal",
+            "admin"
+        ],
+        default: "worker"
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
     }
-  },
-  {
+},
+{
     timestamps: true
-  }
+}
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports =
+mongoose.model(
+    "User",
+    userSchema
+);
